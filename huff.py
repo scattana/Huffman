4baf3afcdc844bc2716a7a808f21a82ca90abc6f
Seth Cattanach <scattanach1@gmail.com>
2020-07-03 16:50:34 -0500

    finished arg parsing

--- huff.py
+++ huff.py
@@ -28,7 +28,7 @@ def encode(f, custom=False):
 
 # display_usage function displays usage instructions for command-line use
 def display_usage():
-	print('\n\n\t' + "USAGE: python huff.py [OPTIONS]")
+	print('\n\t' + "USAGE: python huff.py [OPTIONS]")
 	print('\t' + ''.join('=' for i in range(90)))
 	print("\t{: <20} {: <40}".format("-h", "Display usage instructions"))
 	print("\t{: <20} {: <40}".format("-c [file]", "Compress/encode usage mode (-c or -d is required)"))
@@ -38,7 +38,8 @@ def display_usage():
 	print("\t{: <20} {: <40}".format("-e [encoding]", "Specify input text file encoding. Default is utf-8"))
 	print("\t{: <20} {: <40}".format("-v", "Verbose program output"))
 	print('\n')
-	print("\tThanks for using the Huffman text compressor! We currently support the following character encodings:")
+	print("\tThanks for using the Huffman text compressor!\n\tWe currently support the following character encodings:")
+	print('\t' + ''.join('=' for i in range(60)))
 	with open("supported_encoding_types.txt", "r") as f:
 		print('\n'.join(['\t>> ' + line for line in f.readlines()]))
 	print()
@@ -46,9 +47,78 @@ def display_usage():
 
 # main execution
 def main():
+	# initialize default program parameters
+	input_encoding = "utf-8"
+	optimize = False
+	verbose = False
+
+
+
 	# read command line arguments
 	if len(sys.argv) < 2 or (len(sys.argv) >= 2 and '-h' in sys.argv):
 		display_usage()
+		exit(1)
+	if '-c' not in sys.argv and '-d' not in sys.argv:
+		print("\n\t!! USAGE ERROR: please specify mode (compression or decompression)")
+		display_usage()
+		exit(1)
+	if '-o' in sys.argv:
+		optimize = True
+	if '-v' in sys.argv:
+		verbose = True
+
+	# loop through remaining options, skipping binary flags, and set parameters or display errors appropriately
+	for i in range(len(sys.argv)):
+		if sys.argv[i] in ['-o', '-v', '-g'] or sys.argv[i][0] != '-':
+			continue
+		if sys.argv[i] == '-c':
+			if i+1 >= len(sys.argv):
+				print("\n\t!! USAGE ERROR: please provide a text file for compression")
+				display_usage()
+				exit(1)
+			elif sys.argv[i+1][0] == '-':
+				print("\n\t!! USAGE ERROR: please provide a text file for compression")
+				display_usage()
+				exit(1)
+			else:
+				input_file = sys.argv[i+1]
+				mode = "c"
+		if sys.argv[i] == '-d':
+			if i+1 >= len(sys.argv):
+				print("\n\t!! USAGE ERROR: please provide a text file for decompression")
+				display_usage()
+				exit(1)
+			elif sys.argv[i+1][0] == '-':
+				print("\n\t!! USAGE ERROR: please provide a text file for decompression")
+				display_usage()
+				exit(1)
+			else:
+				input_file = sys.argv[i+1]
+				mode = "d"
+		if sys.argv[i] == '-e':
+			if i+1 >= len(sys.argv):
+				print("\n\t!! USAGE ERROR: -e flag specified, but no encoding provided")
+				display_usage()
+				exit(1)
+			elif sys.argv[i+1][0] == '-':
+				print("\n\t!! USAGE ERROR: -e flag specified, but no encoding provided")
+				display_usage()
+				exit(1)
+			elif sys.argv[i+1] not in [item.strip() for item in open("supported_encoding_types.txt", "r").readlines()]:
+				print("\n\t!! USAGE ERROR: specified encoding is not currently supported")
+				display_usage()
+				exit(1)
+			else:
+				input_encoding = sys.argv[i+1]
+
+
+	# debug print program parameters
+	print("usage params:")
+	print("encoding: " + input_encoding)
+	print("optimize:", optimize)
+	print("verbose:", verbose)
+	print("file: " + input_file)
+
 
 # main function handler
 if __name__ == "__main__":
